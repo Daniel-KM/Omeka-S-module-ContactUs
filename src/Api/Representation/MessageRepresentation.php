4cb5ade9af4b86c667988f1be8c1fd62144a7fcc
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-05-15 00:00:00 +0000

    Fixed and shorted message token.

--- src/Api/Representation/MessageRepresentation.php
+++ src/Api/Representation/MessageRepresentation.php
@@ -230,6 +230,6 @@ class MessageRepresentation extends AbstractEntityRepresentation
     public function token(): ?string
     {
         $string = $this->id() . '/' . $this->email() . '/' . $this->ip() . '/' . $this->userAgent() . '/' . $this->created()->format('Y-m-d H:i:s');
-        return substr(str_replace(['+', '/', '='], '', base64_encode(hash('sha256', $string))), 0, random_int(24, 30));
+        return substr(str_replace(['+', '/', '='], '', base64_encode(hash('sha256', $string))), 0, 12);
     }
 }
