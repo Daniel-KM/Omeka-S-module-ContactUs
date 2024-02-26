815bb3309b2fde1d190492cdf6fa0615ecd869b2
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-02-26 00:00:00 +0000

    Added role guest private.

--- Module.php
+++ Module.php
@@ -30,6 +30,9 @@ class Module extends AbstractModule
         if (!$acl->hasRole('guest')) {
             $acl->addRole('guest');
         }
+        if (!$acl->hasRole('guest_private')) {
+            $acl->addRole('guest_private');
+        }
         $roles = $acl->getRoles();
         $adminRoles = [
             \Omeka\Permissions\Acl::ROLE_GLOBAL_ADMIN,
