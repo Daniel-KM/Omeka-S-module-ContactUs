4c79b77a32dcbeb24ffa873e524ca2d32ac6b6e5
Daniel Berthereau <Daniel.git@Berthereau.net>
2024-11-25 00:00:00 +0000

    Released version 3.4.16 for Omeka S v4.1.

--- data/scripts/upgrade.php
+++ data/scripts/upgrade.php
@@ -12,6 +12,7 @@ use Common\Stdlib\PsrMessage;
  *
  * @var \Omeka\Api\Manager $api
  * @var \Omeka\View\Helper\Url $url
+ * @var \Laminas\Log\Logger $logger
  * @var \Omeka\Settings\Settings $settings
  * @var \Doctrine\DBAL\Connection $connection
  * @var \Doctrine\ORM\EntityManager $entityManager
@@ -20,13 +21,15 @@ use Common\Stdlib\PsrMessage;
 $plugins = $services->get('ControllerPluginManager');
 $url = $services->get('ViewHelperManager')->get('url');
 $api = $plugins->get('api');
+$logger = $services->get('Omeka\Logger');
 $settings = $services->get('Omeka\Settings');
 $translate = $plugins->get('translate');
+$translator = $services->get('MvcTranslator');
 $connection = $services->get('Omeka\Connection');
 $messenger = $plugins->get('messenger');
 $entityManager = $services->get('Omeka\EntityManager');
 
-$configModule = require dirname(__DIR__, 2) . '/config/module.config.php';
+$localConfig = require dirname(__DIR__, 2) . '/config/module.config.php';
 
 if (!method_exists($this, 'checkModuleActiveVersion') || !$this->checkModuleActiveVersion('Common', '3.4.63')) {
     $message = new \Omeka\Stdlib\Message(
@@ -93,7 +96,7 @@ if (version_compare($oldVersion, '3.3.8.4', '<')) {
     $ids = $api->search('sites', [], ['initialize' => false, 'returnScalar' => 'id'])->getContent();
     foreach ($ids as $id) {
         $siteSettings->setTargetId($id);
-        $siteSettings->set('contactus_notify_body', $configModule['contactus']['site_settings']['contactus_notify_body']);
+        $siteSettings->set('contactus_notify_body', $localConfig['contactus']['site_settings']['contactus_notify_body']);
         $siteSettings->set('contactus_notify_subject', $siteSettings->get('contactus_subject'));
         $siteSettings->delete('contactus_subject');
     }
@@ -121,7 +124,7 @@ if (version_compare($oldVersion, '3.3.8.5', '<')) {
         $siteSettings->delete('contactus_newsletter');
         $siteSettings->delete('contactus_newsletter_label');
         $siteSettings->delete('contactus_attach_file');
-        $siteSettings->set('contactus_consent_label', $configModule['contactus']['site_settings']['contactus_consent_label']);
+        $siteSettings->set('contactus_consent_label', $localConfig['contactus']['site_settings']['contactus_consent_label']);
     }
 
     $sql = <<<'SQL'
@@ -175,8 +178,8 @@ ALTER TABLE `contact_message`
 SQL;
     $connection->executeStatement($sql);
 
-    $settings->set('contactus_to_author_subject', $configModule['contactus']['site_settings']['contactus_to_author_subject']);
-    $settings->set('contactus_to_author_body', $configModule['contactus']['site_settings']['contactus_to_author_body']);
+    $settings->set('contactus_to_author_subject', $localConfig['contactus']['site_settings']['contactus_to_author_subject']);
+    $settings->set('contactus_to_author_body', $localConfig['contactus']['site_settings']['contactus_to_author_body']);
 
     $message = new PsrMessage(
         'It’s now possible to set a specific message when contacting author.' // @translate
@@ -273,8 +276,8 @@ if (version_compare($oldVersion, '3.4.14', '<')) {
     $ids = $api->search('sites', [], ['initialize' => false, 'returnScalar' => 'id'])->getContent();
     foreach ($ids as $id) {
         $siteSettings->setTargetId($id);
-        $siteSettings->set('contactus_append_resource_show', $configModule['contactus']['site_settings']['contactus_append_resource_show']);
-        $siteSettings->set('contactus_append_items_browse', $configModule['contactus']['site_settings']['contactus_append_items_browse']);
+        $siteSettings->set('contactus_append_resource_show', $localConfig['contactus']['site_settings']['contactus_append_resource_show']);
+        $siteSettings->set('contactus_append_items_browse', $localConfig['contactus']['site_settings']['contactus_append_items_browse']);
     }
     $message = new PsrMessage(
         'Two new options allow to append the contact form to resource pages. They are disabled by default, so check them if you need them.' // @translate
@@ -293,12 +296,181 @@ if (version_compare($oldVersion, '3.4.15', '<')) {
     $ids = $api->search('sites', [], ['initialize' => false, 'returnScalar' => 'id'])->getContent();
     foreach ($ids as $id) {
         $siteSettings->setTargetId($id);
-        $siteSettings->set('contactus_confirmation_newsletter_subject', $configModule['contactus']['site_settings']['contactus_confirmation_newsletter_subject']);
-        $siteSettings->set('contactus_confirmation_newsletter_body', $configModule['contactus']['site_settings']['contactus_confirmation_newsletter_body']);
+        $siteSettings->set('contactus_confirmation_newsletter_subject', $localConfig['contactus']['site_settings']['contactus_confirmation_newsletter_subject']);
+        $siteSettings->set('contactus_confirmation_newsletter_body', $localConfig['contactus']['site_settings']['contactus_confirmation_newsletter_body']);
     }
 
     $message = new PsrMessage(
         'A new block allows to display a form to subscribe to a newsletter.' // @translate
+    );
+    $messenger->addSuccess($message);
+}
+
+if (version_compare($oldVersion, '3.4.16', '<')) {
+    /**
+     * Migrate blocks of this module to new blocks of Omeka S v4.1.
+     *
+     * Migrate templates.
+     * Replace filled settting "heading" by a specific block "Heading" or "Html".
+     *
+     * @var \Laminas\Log\Logger $logger
+     *
+     * @see \Omeka\Db\Migrations\MigrateBlockLayoutData
+     */
+
+    // It is not possible to search for templates that use heading, because it
+    // is used in many places. So only in doc block.
+    // Warning: heading is no more used in blocks, but still usable in the view
+    // helper.
+
+    // Check themes that use "$heading" in block
+    $strings = [
+        'themes/*/view/common/block-layout/contact-us*' => [
+            '* @var string $heading',
+            'if ($options[\'heading\'])',
+        ],
+        'themes/*/view/common/block-template/contact-us*' => [
+            '* @var string $heading',
+            'if ($options[\'heading\'])',
+        ],
+    ];
+    $manageModuleAndResources = $this->getManageModuleAndResources();
+    $results = [];
+    foreach ($strings as $path => $strings) {
+        $result = $manageModuleAndResources->checkStringsInFiles($strings, $path);
+        if ($result) {
+            $results[] = $result;
+        }
+    }
+    if ($results) {
+        $message = new PsrMessage(
+            'The option "heading" was removed from blocks Contact Us and Newsletter and replaced by a block Heading (if module Block Plus is present) or Html. Remove it in the following files before upgrade and automatic conversion: {json}', // @translate
+            ['json' => json_encode($results, 448)]
+        );
+        $logger->err($message->getMessage(), $message->getContext());
+        throw new \Omeka\Module\Exception\ModuleCannotInstallException((string) $message->setTranslator($translator));
+    }
+
+    $pageRepository = $entityManager->getRepository(\Omeka\Entity\SitePage::class);
+
+    $viewHelpers = $services->get('ViewHelperManager');
+    $escape = $viewHelpers->get('escapeHtml');
+    $hasBlockPlus = $this->isModuleActive('BlockPlus');
+
+    $pagesUpdated = [];
+    $pagesUpdated2 = [];
+    foreach ($pageRepository->findAll() as $page) {
+        $pageSlug = $page->getSlug();
+        $siteSlug = $page->getSite()->getSlug();
+        $position = 0;
+        foreach ($page->getBlocks() as $block) {
+            $block->setPosition(++$position);
+            $layout = $block->getLayout();
+            if ($layout !== 'contactUs' && $layout !== 'newsletter') {
+                continue;
+            }
+            $data = $block->getData() ?: [];
+            $layoutData = $block->getLayoutData() ?? [];
+
+            // Migrate template.
+            $template = $data['template'] ?? '';
+            $layoutData = $block->getLayoutData() ?? [];
+            $existingTemplateName = $layoutData['template_name'] ?? null;
+            $templateName = pathinfo($template, PATHINFO_FILENAME);
+            $templateCheck = $layout === 'newsletter' ? 'newsletter' : 'contact-us';
+            if ($templateName
+                && $templateName !== $templateCheck
+                && (!$existingTemplateName || $existingTemplateName === $templateCheck)
+            ) {
+                $layoutData['template_name'] = $templateName;
+                $pagesUpdated[$siteSlug][$pageSlug] = $pageSlug;
+            }
+            unset($data['template']);
+
+            $heading = $data['heading'] ?? '';
+
+            // Replace setting "heading".
+            if (strlen($heading)) {
+                $b = new \Omeka\Entity\SitePageBlock();
+                $b->setPage($page);
+                $b->setPosition(++$position);
+                if ($hasBlockPlus) {
+                    $b->setLayout('heading');
+                    $b->setData([
+                        'text' => $heading,
+                        'level' => 3,
+                    ]);
+                } else {
+                    $b->setLayout('html');
+                    $b->setData([
+                        'html' => '<h3>' . $escape($heading) . '</h3>',
+                    ]);
+                }
+                $entityManager->persist($b);
+                $block->setPosition(++$position);
+                $pagesUpdated2[$siteSlug][$pageSlug] = $pageSlug;
+            }
+            unset($data['heading']);
+
+            $block->setData($data);
+            $block->setLayoutData($layoutData);
+        }
+    }
+
+    $entityManager->flush();
+
+    if ($pagesUpdated) {
+        $result = array_map('array_values', $pagesUpdated);
+        $message = new PsrMessage(
+            'The setting "template" was moved to the new block layout settings available since Omeka S v4.1. You may check pages for styles: {json}', // @translate
+            ['json' => json_encode($result, 448)]
+        );
+        $messenger->addWarning($message);
+        $logger->warn($message->getMessage(), $message->getContext());
+    }
+
+    if ($pagesUpdated2) {
+        $result = array_map('array_values', $pagesUpdated2);
+        $message = new PsrMessage(
+            'The option "heading" was removed from blocks Contact Us and Newsletter. New block "Heading" (if module Block Plus is present) or "Html" was prepended to all blocks that had a filled heading. You may check pages for styles: {json}', // @translate
+            ['json' => json_encode($result, 448)]
+        );
+        $messenger->addWarning($message);
+        $logger->warn($message->getMessage(), $message->getContext());
+    }
+
+    $siteSettings = $services->get('Omeka\Settings\Site');
+    $siteIds = $api->search('sites', [], ['returnScalar' => 'id'])->getContent();
+    foreach ($siteIds as $siteId) {
+        $siteSettings->setTargetId($siteId);
+        if (!$siteSettings->get('contactus_confirmation_message')) {
+            $siteSettings->set('contactus_confirmation_message', $localConfig['contactus']['site_settings']['contactus_confirmation_message']);
+        }
+        if (!$siteSettings->get('contactus_confirmation_message_newsletter')) {
+            $siteSettings->set('contactus_confirmation_message_newsletter', $localConfig['contactus']['site_settings']['contactus_confirmation_message_newsletter']);
+        }
+        if ($siteSettings->get('contactus_selection_max') === null) {
+            $siteSettings->set('contactus_selection_max', 25);
+        }
+    }
+
+    $message = new PsrMessage(
+        'New options were added to set message after posting mail.' // @translate
+    );
+    $messenger->addSuccess($message);
+
+    $message = new PsrMessage(
+        'It is now possible to unsubscribe to a newsletter.' // @translate
+    );
+    $messenger->addSuccess($message);
+
+    $message = new PsrMessage(
+        'A new page allows the user or the visitor to see all the selected resources.' // @translate
+    );
+    $messenger->addSuccess($message);
+
+    $message = new PsrMessage(
+        'The number of selected items can be set in site settings. It is limited to 25 by default.' // @translate
     );
     $messenger->addSuccess($message);
 }
